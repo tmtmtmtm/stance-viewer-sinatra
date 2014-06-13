f5ea65950f0743c97d08391a4c7b38db90aeb901
Tony Bowden <tony@tmtm.com>
2014-06-13 14:36:55 +0300

    party.html

--- app.rb
+++ app.rb
@@ -2,12 +2,6 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
-helpers do
-  def json_file(file)
-    JSON.parse(File.read("data/#{file}.json"))
-  end
-end
-
 get '/' do
   haml :index
 end
@@ -33,7 +27,19 @@ get '/about.html' do
 end
 
 get '/party/:id' do |id|
-  id
+  parties = json_file('parties')
+  @party = parties.detect { |p| p['id'] == id } or pass
+  @members = json_file('people').find_all { |mp| 
+    mp['memberships'].detect { |mem| mem['organization_id'] == id } 
+  }
+  @stances = json_file('partystances').map { |s|
+    s['stances'][id].merge({
+      "id" => s['id'],
+      "text" => s['html'],
+      "stance_text" => stance_text(s['stances'][id]),
+    })
+  }
+  haml :party
 end
 
 get '/person/:id' do |id|
@@ -44,6 +50,24 @@ get '/issue/:id' do |id|
   id
 end
 
+
+helpers do
+  def json_file(file)
+    JSON.parse(File.read("data/#{file}.json"))
+  end
+
+  def stance_text(stance)
+    return "has never voted on"           if stance['num_votes'].zero?
+    return "very strongly for"            if stance['weight'] > 0.95
+    return "strongly for"                 if stance['weight'] > 0.85
+    return "moderately for"               if stance['weight'] > 0.6
+    return "a mixture of for and against" if stance['weight'] > 0.4
+    return "moderately against"           if stance['weight'] > 0.15
+    return "strongly against"             if stance['weight'] > 0.05
+    return "very strongly against"        
+  end
+
+end
 
 
 
