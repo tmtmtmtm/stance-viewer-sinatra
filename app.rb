bf6997263d01056ab38b1f8b1644e398188ca0ef
Tony Bowden <tony@tmtm.com>
2014-06-13 12:12:59 +0300

    Skeleton pages
    Pages to be fleshed out

--- app.rb
+++ app.rb
@@ -3,3 +3,35 @@ require 'sinatra'
 get '/' do
   "hello world"
 end
+
+get '/parties.html' do
+  "Political parties"
+end
+
+get '/people.html' do
+  "Members of Parliament"
+end
+
+get '/issues.html' do
+  "Issues"
+end
+
+get '/about.html' do
+  "About"
+end
+
+get '/party/:id' do |id|
+  id
+end
+
+get '/person/:id' do |id|
+  id
+end
+
+get '/issue/:id' do |id|
+  id
+end
+
+
+
+
