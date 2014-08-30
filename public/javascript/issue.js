8075903a058b3bc9d64f11cba3a27107fb492c12
Tony Bowden <tony@tmtm.com>
2014-08-30 14:52:04 +0300

    Move from RHS to LHS on click

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -2,7 +2,8 @@
 $(document).ready(function() {
     console.log("Ready");
     $('#motionList button').click( function(e) {
-        alert('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
+        // console.log('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
+        $(this).parent('li').appendTo('#selected-'+$(this).attr('data'));
     });
 });
 
