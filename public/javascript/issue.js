b53fef8472265b4f246c33fa4531fa29bbf2384d
Tony Bowden <tony@tmtm.com>
2014-08-30 14:38:10 +0300

    remove hg-click() on RHS motion buttons

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
     console.log("Ready");
     $('#motionList button').click( function(e) {
-        alert('ID: ' + $(this).parent().attr('id'));
+        alert('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
     });
 });
 
