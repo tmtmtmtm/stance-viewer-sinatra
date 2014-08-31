129bdcc0b5ac12254f543895b2b66f63556e493a
Tony Bowden <tony@tmtm.com>
2014-08-31 13:40:52 +0300

    rejig the buggons

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -3,27 +3,33 @@ $(document).ready(function() {
     console.log("Ready");
 
     // TODO make the buttons do something
-    function strengthButtonGroup(direction) {
-        return $("<div>").addClass("strengthButtons btn-group pull-right").append(
-          $("<label>").addClass("btn btn-default btn-xs").text("Strong")
-        ).append(
-          $("<label>").addClass("btn btn-default btn-xs").text("Moderate")
-        );
+    function strengthButtonGroup() {
+        return $("<div>")
+            .addClass("strengthButtons btn-group pull-right")
+            .append( $("<label>").addClass("btn btn-default btn-xs").text("Strong") )
+            .append( $("<label>").addClass("btn btn-default btn-xs").text("Moderate") );
     }
-  
-    function motion_html(id, name) { 
-        return $("<li>").attr('id', id).addClass("list-group-item").text(name).append(strengthButtonGroup('for')).append($("<br>")).append(
-            $("<button>").attr('type', 'button').attr('data', 'for').addClass("btn btn-primary").text("For")
+
+    function chooseButtonGroup() { 
+        return $("<div>").addClass("chooseButtons").append(
+          $("<button>").attr('type', 'button').attr('data', 'for').addClass("btn btn-primary").text("For")
         ).append(" ").append(
             $("<button>").attr('type', 'button').attr('data', 'against').addClass("btn btn-danger").text("Against")
         );
     }
+  
+    function motion_html(id, name) { 
+        return $("<li>").attr('id', id).addClass("list-group-item motion").text(name)
+          .append( strengthButtonGroup() )
+          .append( chooseButtonGroup() );
+    }
 
     
     function make_search_results_movable() { 
-      $('#motionList button').click( function(e) {
+      $('.chooseButtons button').click( function(e) {
           var dir = $(this).attr('data');
-          var m = $(this).parent('li')
+          console.log("Adding to " + dir);
+          var m = $(this).parents('li.motion')
           m.appendTo('#selected-'+dir);
           m.prepend( $("<span>").addClass("glyphicon glyphicon-minus-sign actionable").text(" "));
           m.find('button').hide();
@@ -48,6 +54,8 @@ $(document).ready(function() {
                   console.log(motion['id'] + ": " + motion['text'])
                 });
                 make_search_results_movable();
+                console.log("Have: " + $("#issue-motions li").length );
+                // $("#no-motions-yet").hide() 
             }
         });
         e.preventDefault();
