059581b83ac0d078f613c401908bada0ef07d24d
Tony Bowden <tony@tmtm.com>
2014-08-31 12:12:35 +0300

    Redecorate with strength buttons

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -2,13 +2,25 @@
 $(document).ready(function() {
     console.log("Ready");
 
+    // TODO make the buttons do something
+    function strengthButtonGroup(direction) {
+        return '<div class="btn-group pull-right">' +
+            '<label class="btn btn-default btn-xs" btn-radio="2">Strongly ' + direction + '</label>' +
+            '<label class="btn btn-default btn-xs" btn-radio="1">Moderately ' + direction + '</label>' +
+          '</div>';
+    }
+  
+
     function make_search_results_movable() { 
       $('#motionList button').click( function(e) {
-          // console.log('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
+          var dir = $(this).attr('data');
+          // console.log('ID: ' + $(this).parent().attr('id') + ": " + dir);
           var m = $(this).parent('li')
-          m.appendTo('#selected-'+$(this).attr('data'));
+          m.appendTo('#selected-'+dir);
           m.prepend( $("<span>").addClass("glyphicon glyphicon-minus-sign actionable").text(" "));
-          // next ... change the decoration when we move
+          m.children('button').remove();
+          m.children('br').remove();
+          m.append(strengthButtonGroup(dir));
           e.preventDefault();
       });
     }
