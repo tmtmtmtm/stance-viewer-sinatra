911aeaf2334ef7b861798d55841a7c2481b8c51f
Tony Bowden <tony@tmtm.com>
2014-08-31 10:56:27 +0300

    search box makes ajax call
    For now, just console.log the results

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
     $('#motionList button').click( function(e) {
         // console.log('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
         $(this).parent('li').appendTo('#selected-'+$(this).attr('data'));
+        // next ... change the decoration when we move
+    });
+
+    $("#motion-search-form").submit(function() {
+        $.ajax({
+            type: "GET",
+            url: "/api/motions",
+            data: $("#motion-search-form").serialize(),
+            success: function(data) {
+                console.log(data);
+            }
+        });
+        return false; 
     });
 });
 
