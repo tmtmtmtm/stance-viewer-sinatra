a1c2d09e2ec3dee2b8d39ed58431e4e8129ca69a
Tony Bowden <tony@tmtm.com>
2014-08-31 11:31:30 +0300

    Populate search results from ajax

--- public/javascript/issue.js
+++ public/javascript/issue.js
@@ -1,22 +1,41 @@
 
 $(document).ready(function() {
     console.log("Ready");
-    $('#motionList button').click( function(e) {
-        // console.log('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
-        $(this).parent('li').appendTo('#selected-'+$(this).attr('data'));
-        // next ... change the decoration when we move
-    });
 
-    $("#motion-search-form").submit(function() {
+    function make_search_results_movable() { 
+      $('#motionList button').click( function(e) {
+          // console.log('ID: ' + $(this).parent().attr('id') + ": " + $(this).attr('data'));
+          $(this).parent('li').appendTo('#selected-'+$(this).attr('data'));
+          // next ... change the decoration when we move
+          e.preventDefault();
+      });
+    }
+
+    function motion_html(id, name) { 
+        return $("<li>").attr('id', id).addClass("list-group-item").text(name).append($("<br>")).append(
+            $("<button>").attr('type', 'button').attr('data', 'for').addClass("btn btn-primary").text("For")
+        ).append(" ").append(
+            $("<button>").attr('type', 'button').attr('data', 'against').addClass("btn btn-danger").text("Against")
+        );
+    }
+
+    make_search_results_movable();
+    $("#motion-search-form").submit(function(e) {
+        $("ul#motionList li").remove();
         $.ajax({
             type: "GET",
             url: "/api/motions",
             data: $("#motion-search-form").serialize(),
             success: function(data) {
-                console.log(data);
+                $("ul#motionList li").remove();
+                jQuery.each(data, function(i, motion) {
+                  $("ul#motionList").append( motion_html(motion['id'], motion['text']) );
+                  console.log(motion['id'] + ": " + motion['text'])
+                });
+                make_search_results_movable();
             }
         });
-        return false; 
+        e.preventDefault();
     });
 });
 
